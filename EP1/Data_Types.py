161135548d9cf68ffcc04f86143f49a01615b55b
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-03-01 15:37:46 +0700

    Data_Types

--- EP1/Data_Types.py
+++ EP1/Data_Types.py
@@ -1,4 +1,21 @@
+from unittest import result
+
+
 a = 20
 b = 15.5
 c = -5739
-print(type(a),type(b),type(c))
+print(a,b,c) # type() to Cheak Type value 
+
+
+num1 = 50 #integer
+num2 = 10.5 #float
+result = num1 + num2
+print(result)
+
+a= "a" #single Line String
+print(a)
+
+a=""" good morning
+dsadsadsadsa dsa  """ #multiline Strings
+
+print(a)
