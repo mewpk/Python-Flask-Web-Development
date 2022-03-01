8c459be494bef9a6275b167bc4cdb01e4f22f580
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-03-01 18:02:35 +0700

    loop

--- Python Introduction/Operators.py
+++ Python Introduction/Operators.py
@@ -3,3 +3,20 @@
 # **
 # * / // %
 # + -
+val = 10 
+val += 10 # val = val + 10
+print(val)
+if val > 10: 
+    print(True)
+
+inp_point = int(input("type your point here :"))
+if inp_point >= 80:
+    print("Grade A")
+elif inp_point >= 70:
+    print("Grade B")
+elif inp_point >= 60:
+    print("Grade C")
+elif inp_point >= 50:
+    print("Grade D")
+else : 
+    print("Grade F")
---
+++ Python Introduction/loop.py
@@ -0,0 +1,12 @@
+python= ["A","B","C"]
+
+for py in python :
+    print(py)
+
+# range(start, stop[, step])
+for i in range(0,5,2):
+    print(i)
+x=0
+while x<10:
+    x += 1
+    print(x) 
