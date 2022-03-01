0005efd1fbce74b69b7eb6ccfd4e079d10543a45
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-03-01 19:16:52 +0700

    json_flies

--- Python Introduction/json_test.py
+++ Python Introduction/json_test.py
@@ -1,5 +1,6 @@
 import json
 
+# JSON TO PYOBJ
 test = """ {
     "name": "Son",
     "age" : 25,
@@ -9,7 +10,7 @@ test = """ {
 j_test = json.loads(test)
 print(j_test)
 
-
+# PYOBJ TO  JSON 
 test ={
     "name": "Son",
     "age" : 25,
@@ -19,3 +20,13 @@ test ={
 j_test = json.dumps(test,indent=4)
 
 print(j_test)
+
+
+pt_dict = {
+    "page" : "marianonilne",
+    "followers" : 1000,
+    "partners" : None
+}
+#open("name files","w ?? write "r" ?? read")
+with open("pages_info.json","w") as json_flie:
+    data = json.dump(pt_dict,json_flie,indent=4)
